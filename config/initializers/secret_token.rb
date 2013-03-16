ba711a7da89bfcd62853d714154d588dd7f354cd
Renganathan Ramamoorthy <gurupanguji@gmail.com>
2013-03-16 14:31:54 -0700

    First git commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foobar::Application.config.secret_token = '9e7efbc554472e21f207f0a08608ebf4d78c9be7c6bf264ee9db585138f5657c7e91b015182c36836f4c75272a02a866646a514479deb611962d168346ca9f39'
