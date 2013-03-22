17d8ab9ba31f3beccf4ba8225f55aabea31b34ba
Renganathan Ramamoorthy <gurupanguji@gmail.com>
2013-03-21 22:06:37 -0700

    Production requirements - Try 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,9 @@ Foobar::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Production -- Change it to the corresponding heroku title
+  config.action_mailer.default_url_options = { :host => 'http://mysterious-spire-8416.herokuapp.com' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -64,8 +67,5 @@ Foobar::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
-  # Production -- Change it to the corresponding heroku title
-  config.action_mailer.default_url_options = { :host => 'http://mysterious-spire-8416.herokuapp.com' }
 
 end
