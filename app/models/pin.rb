5e7bc474ca9bd7b989631177fa178c44c083fecf
Renganathan Ramamoorthy <gurupanguji@gmail.com>
2013-03-23 13:50:29 -0700

    Fixing some bugs for Pin associations:

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,5 +5,5 @@ class Pin < ActiveRecord::Base
   
   # Associate with user - n:1 relationship
   belongs_to :user
-  validates :user_id, presennce: true #makes sure that if user_id is not present, it won't save to database. 
+  validates :user_id, presence: true #makes sure that if user_id is not present, it won't save to database. 
 end
